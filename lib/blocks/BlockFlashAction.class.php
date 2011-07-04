afd3bafd2459217031c7b55e8e5c8cf9714ae732
Eric HAUSWALD <eric.hauswald@rbs.fr>
2011-07-04 17:24:20 +0200

    [FIX] Correction prévisualisation backoffice des blocs

--- lib/blocks/BlockFlashAction.class.php
+++ lib/blocks/BlockFlashAction.class.php
@@ -18,7 +18,7 @@ class media_BlockFlashAction extends website_BlockAction
 	 */
 	public function execute($request, $response)
 	{
-		if ($this->isInBackoffice())
+		if ($this->isInBackofficeEdition())
 		{
 			return $this->executeBackOffice($request, $response);
 		}

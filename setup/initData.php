7aa3d00309e4fc25ebdc5963f476913a8232cdea
Gaël PORT <gael.port@rbs.fr>
2012-09-19 17:44:33 +0200

    [DELETE] Delete old samples.

--- setup/initData.php
+++ setup/initData.php
@@ -3,16 +3,7 @@ class media_Setup extends object_InitDataSetup
 {
 	public function install()
 	{
-		try
-		{
-			$scriptReader = import_ScriptReader::getInstance();
-			$scriptReader->executeModuleScript('media', 'init.xml');
-		}
-		catch (Exception $e)
-		{
-			echo "ERROR: " . $e->getMessage() . "\n";
-			Framework::exception($e);
-		}
+		$this->executeModuleScript('init.xml');
 
 		media_ModuleService::getInstance()->addRemoveTmpFilesTask();
 	}
